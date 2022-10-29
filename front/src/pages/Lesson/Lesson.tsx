d8a7401690d85633daa79c413aa575284f8b4a00
Sergey Prilepko <sergey.prilepcko@gmail.com>
2022-10-30 00:20:44 +0300

    PE-16: some texts and field for message

--- front/src/pages/Lesson/Lesson.tsx
+++ front/src/pages/Lesson/Lesson.tsx
@@ -1,12 +1,27 @@
+import { Typography, Stack, TextField } from '@mui/material';
+
 function Lesson() {
     return (
-        <iframe
-            width="420"
-            height="315"
-            src="https://www.youtube.com/embed//w8QbsPx73nU?autoplay=1"
-            frameBorder="0"
-            allowFullScreen
-        ></iframe>
+        <Stack spacing={2}>
+            <Typography variant="h1">Lesson: 1</Typography>
+            <Typography variant="subtitle1">Description</Typography>
+            <iframe
+                title="lesson_from_youtube"
+                width="420"
+                height="315"
+                src="https://www.youtube.com/embed//w8QbsPx73nU?autoplay=1"
+                frameBorder="0"
+                allowFullScreen
+            />
+            <Typography variant="subtitle1">Homework</Typography>
+            <TextField
+                id="outlined-multiline-static"
+                label="Write here your homework or comments"
+                multiline
+                rows={12}
+                defaultValue="..."
+            />
+        </Stack>
     );
 }
 
