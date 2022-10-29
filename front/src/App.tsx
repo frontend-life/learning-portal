d15ed0b56c15e28a99409334e55623f735e45fa2
Sergey Prilepko <sergey.prilepcko@gmail.com>
2022-10-29 23:38:16 +0300

    PE-16: iframe youtube fullscreen

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { AddLesson } from './pages/AddLesson/AddLesson';
+import Lesson from './pages/Lesson/Lesson';
 
 function App() {
-  return (
-    <div className="App">
-      <AddLesson />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Lesson />
+        </div>
+    );
 }
 
 export default App;
---
+++ front/src/pages/Lesson/Lesson.tsx
@@ -0,0 +1,13 @@
+function Lesson() {
+    return (
+        <iframe
+            width="420"
+            height="315"
+            src="https://www.youtube.com/embed//w8QbsPx73nU?autoplay=1"
+            frameBorder="0"
+            allowFullScreen
+        ></iframe>
+    );
+}
+
+export default Lesson;
