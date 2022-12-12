299911294b2e8ba55f30085ff0da0ceb8aa4e9fe
gurbinder <gsgurbinder12@gmail.com>
2022-12-12 20:44:37 +0530

    edit 2

--- node_server/src/router/user.ts
+++ node_server/src/router/user.ts
@@ -13,7 +13,7 @@ import { signinUserDTO } from "../dto/signinUserDTO";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send("Hello its workng");
+  res.send("Hello its workng & I am testing again");
 });
 
 router.post("/user/signup", async (req, res) => {
