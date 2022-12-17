d21a5f5c1e5647431ba81cbf06b23c2f0659482e
Sergey Prilepko <sergey.prilepcko@gmail.com>
2022-12-17 20:54:04 +0300

    DEV: .gitignore + delete dist and public fron node_modules

--- node_server/dist/service/apiBuilder.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDTORouter = void 0;
-// function to create REST for new DTO
-const createDTORouter = () => { };
-exports.createDTORouter = createDTORouter;
--- node_server/dist/service/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles[Roles["TEACHER"] = 0] = "TEACHER";
-    Roles[Roles["STUDENT"] = 1] = "STUDENT";
-})(Roles = exports.Roles || (exports.Roles = {}));
