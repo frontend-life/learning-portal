830bfbcbfe991110d1e1fc6132d8b09893c06c3c
Sergey Prilepko <sergey.prilepcko@gmail.com>
2022-12-24 00:08:49 +0300

    DEV: add prefix api for api requessts

--- front/src/utils/axios.ts
+++ front/src/utils/axios.ts
@@ -3,16 +3,20 @@ import axios from 'axios';
 import { addErrorNt } from './notification';
 
 const NO_TOKEN_ERROR = 'No token';
+const API_PREFIX = 'api';
 
 export const getPort = () => 3001;
 
-export const getBaseUrl = () =>
-    process.env.NODE_ENV === 'production'
+export const getBaseApiUrl = () => `${getBaseUrl()}/${API_PREFIX}`;
+
+export const getBaseUrl = () => {
+    return process.env.NODE_ENV === 'production'
         ? ''
         : `http://localhost:${getPort()}`;
+};
 
 export const myRequest = axios.create({
-    baseURL: getBaseUrl(),
+    baseURL: getBaseApiUrl(),
     timeout: 1000
 });
 
@@ -60,6 +64,6 @@ myRequest.interceptors.response.use(
 );
 
 export const API_URLS = {
-    SIGN_IN: '/user/signin',
-    SIGN_UP: '/user/signup'
+    SIGN_IN: `/${API_PREFIX}/user/signin`,
+    SIGN_UP: `/${API_PREFIX}/user/signup`
 };
