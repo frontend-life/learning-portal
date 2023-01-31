b2fa23fa6d23afa47ec376b3812b2b1e6c236ca2
Sergey Prilepko <sergey.prilepcko@gmail.com>
2023-01-31 19:30:16 +0300

    DEV: sending messages to telegram

--- node_server/src/router/message.ts
+++ node_server/src/router/message.ts
@@ -6,7 +6,7 @@ import express from "express";
 import { Chat } from "../models/chat";
 import { IMessage, Message } from "../models/message";
 import { sendMessageToChat } from "./events/chatEvents";
-import { User } from "../models/user";
+import { IUser, User } from "../models/user";
 
 const router = express.Router();
 
@@ -36,17 +36,26 @@ router
       saved_new_message = await saved_new_message.populate("attachments");
 
       chat.messages = [...chat.messages, saved_new_message._id.toString()];
-      chat.participants = Array.from(new Set([...chat.participants, senderId]));
+      chat.participants = Array.from(
+        new Set([...chat.participants.map((p) => p.toString()), senderId])
+      );
 
       await chat.save();
 
-      const users = await chat.populate("participants");
+      const { participants } = await chat.populate("participants");
+      const chat_ids = (participants as unknown as IUser[])
+        .filter(({ _id }) => {
+          return _id.toString() !== senderId;
+        })
+        .map(({ telegramChatId }) => telegramChatId);
 
-      nofityAllParticipantsThroughTelegram(
-        // @ts-ignore
-        Array.from(users).map(({ telegramChatId }) => telegramChatId),
-        text || "only attachments"
-      );
+      if (chat_ids.length) {
+        nofityAllParticipantsThroughTelegram(
+          chat_ids,
+          text || "only attachments"
+        );
+      }
+
       sendMessageToChat(chatId, senderId, saved_new_message);
 
       try {
