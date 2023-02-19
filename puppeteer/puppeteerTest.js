08124f0d0442960ceae8c685f0200d9efd54373a
Sergey Prilepko <sergey.prilepcko@gmail.com>
2023-02-19 13:03:45 +0300

    TEST: puppeteer npm init + checking styles example

--- puppeteer/puppeteerTest.js
+++ puppeteer/puppeteerTest.js
@@ -4,59 +4,44 @@ const path = require("path");
 
 const timeLabel = "Puppeteer: check html for H1";
 
-const main = async () => {
-  // console.time(timeLabel);
+async function checkHtmlPageByPuppeteer() {
   const contentHtml = fs.readFileSync(
     path.resolve(__dirname, "./testPuppeteer.html"),
     "utf8"
   );
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   page.setDefaultTimeout(100);
   await page.setContent(contentHtml);
 
+  const headerSelector = "h1";
   try {
-    const headerSelector = "h1";
-    const h1 = await page.waitForSelector(headerSelector);
-    console.log(await (await h1.getProperty("textContent")).jsonValue());
+    const [isRed, isDisplayFlex] = await page.$eval(headerSelector, (el) => {
+      const styles = window.getComputedStyle(el);
+      return [styles.color === "rgb(255, 0, 0)", styles.display === "flex"];
+    });
+
+    console.log(isRed, isDisplayFlex);
   } catch (err) {
     console.log("Error -> no element");
   }
-  // console.timeEnd(timeLabel);
 
-  /**
-   * Example from docs
-   
-  //   await page.goto("https://developer.chrome.com/");
+  // const spanSelector = "span";
+  // try {
+  //   const span = await page.waitForSelector(spanSelector);
+  // } catch (err) {
+  //   console.log("Error -> no element");
+  // }
 
-  // Set screen size
-  //   await page.setViewport({ width: 1080, height: 1024 });
+  // const imgSelector = "img";
+  // try {
+  //   const img = await page.waitForSelector(imgSelector);
+  // } catch (err) {
+  //   console.log("Error -> no element");
+  // }
 
-  // Type into search box
-//   await page.type(".search-box__input", "automate beyond recorder");
-
-  // Wait and click on first result
-  const searchResultSelector = ".search-box__link";
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    "text/Customize and automate"
-  );
-
-  const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-*/
-  //   await (() =>
-  //     new Promise((res) => {
-  //       setTimeout(() => res(), 5000);
-  //     }))();
   await browser.close();
-};
+}
 
 async function checkTimeAsyncFunc(func, label) {
   console.time(label);
@@ -74,5 +59,4 @@ async function checkMemoryAsyncFunc(func) {
   }
 }
 
-checkTimeAsyncFunc(main, timeLabel);
-checkMemoryAsyncFunc(main, timeLabel);
+checkHtmlPageByPuppeteer();
