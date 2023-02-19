443b54d806b5d2ef09b294c36b62739b479894d4
Sergey Prilepko <sergey.prilepcko@gmail.com>
2023-02-19 13:32:59 +0300

    TEST: puppeteer util function to log results + check possibility to use css external file with styles in tests

--- puppeteer/puppeteerTest.js
+++ puppeteer/puppeteerTest.js
@@ -9,38 +9,88 @@ async function checkHtmlPageByPuppeteer() {
     path.resolve(__dirname, "./testPuppeteer.html"),
     "utf8"
   );
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   page.setDefaultTimeout(100);
   await page.setContent(contentHtml);
 
+  /**
+   *  Test css styles: flex and color
+   */
   const headerSelector = "h1";
   try {
-    const [isRed, isDisplayFlex] = await page.$eval(headerSelector, (el) => {
+    const result = await page.$eval(headerSelector, (el) => {
       const styles = window.getComputedStyle(el);
-      return [styles.color === "rgb(255, 0, 0)", styles.display === "flex"];
+      const isRed = styles.color === "rgb(255, 0, 0)";
+      const isDisplayFlex = styles.display === "flex";
+      return { isRed, isDisplayFlex };
     });
 
-    console.log(isRed, isDisplayFlex);
+    logCheckResult(result);
   } catch (err) {
     console.log("Error -> no element");
   }
 
-  // const spanSelector = "span";
-  // try {
-  //   const span = await page.waitForSelector(spanSelector);
-  // } catch (err) {
-  //   console.log("Error -> no element");
-  // }
+  /**
+   * Test css styles with external css file: width, height, background-color
+   */
+  try {
+    await page.addStyleTag({
+      path: path.resolve(__dirname, "./testPuppeteer.css"),
+    });
+    const result = await page.$eval(".content", (el) => {
+      const isElExists = el !== null;
+      const elStyles = window.getComputedStyle(el);
+      const isWidthCorrect = elStyles.width === "400px";
+      const isHeightCorrect = elStyles.height === "400px";
+      const isBackgroundColorCorrect =
+        elStyles.backgroundColor === "rgb(127, 255, 212)";
 
-  // const imgSelector = "img";
-  // try {
-  //   const img = await page.waitForSelector(imgSelector);
-  // } catch (err) {
-  //   console.log("Error -> no element");
-  // }
+      return {
+        isElExists,
+        isWidthCorrect,
+        isHeightCorrect,
+        isBackgroundColorCorrect,
+      };
+    });
+
+    logCheckResult(result);
+  } catch (err) {
+    console.log("Error -> no element");
+  }
+
+  const spanSelector = "span";
+  try {
+    const span = await page.waitForSelector(spanSelector);
+  } catch (err) {
+    console.log("Error -> no element");
+  }
+
+  const imgSelector = "img";
+  try {
+    const img = await page.waitForSelector(imgSelector);
+  } catch (err) {
+    console.log("Error -> no element");
+  }
+
+  /**
+   * Timeout to see browser if it is turn on in headless mode
+   */
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
   await browser.close();
+}
+
+/**
+ * result is an object with keys: isRed, isDisplayFlex, for example
+ * as return we got string like this: "Test is success -> isRed: true, isDisplayFlex: true"
+ */
+function logCheckResult(result) {
+  console.log(
+    `Test is success -> ${Object.entries(result).reduce((acc, [key, value]) => {
+      return acc + `${key}: ${value}, `;
+    }, "")})}`
+  );
 }
 
 async function checkTimeAsyncFunc(func, label) {
@@ -59,4 +109,4 @@ async function checkMemoryAsyncFunc(func) {
   }
 }
 
-checkHtmlPageByPuppeteer();
+checkMemoryAsyncFunc(checkHtmlPageByPuppeteer);
